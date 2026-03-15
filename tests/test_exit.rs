0ac7bd5b52a2f77f8aa68c90445cd57e4d5644c8
Mike Foster <i.am.mike.foster@gmail.com>
2026-03-15 13:08:48 +0000

    add integration test

---
+++ tests/test_exit.rs
@@ -0,0 +1,32 @@
+#![feature(assert_matches)]
+#![feature(never_type)]
+#![feature(try_trait_v2)]
+
+use std::process::Termination;
+use std::assert_matches::assert_matches;
+
+use try_v2::TryTraitv2;
+
+#[derive(Debug, TryTraitv2)]
+enum Exit<T: Termination> {
+    Ok(T),
+    TestsFailed,
+}
+
+#[test]
+fn short_circuit() {
+    fn fail() -> Exit<()> {
+        Exit::TestsFailed?;
+        Exit::Ok(())
+    }
+    assert_matches!(fail(), Exit::TestsFailed)
+}
+
+#[test]
+fn no_short_circuit() {
+    fn pass() -> Exit<()> {
+        Exit::Ok(())?;
+        Exit::Ok(())
+    }
+    assert_matches!(pass(), Exit::Ok(()))
+}
