c486cd8c547bebb1f358a1ddebada2808c2f0dff
Mike Foster <i.am.mike.foster@gmail.com>
2026-03-15 15:07:25 +0000

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,56 @@
+//! Provides a derive macro for `Try`
+//! ([try_trait_v2](https://rust-lang.github.io/rfcs/3058-try-trait-v2.html))
+//!
+//! ## Requires:
+//!   - `RUSTC_BOOTSRAP = 1` (or nightly)
+//!   - `#![feature(never_type)]`
+//!   - `#![feature(try_trait_v2)]`
+//!
+//! ## Current Limitations on the annotated type:
+//!   - must be an `enum`
+//!   - must have _at least one_ generic type
+//!   - the _first_ generic type must be the `Output` type (produced when not short circuiting)
+//!   - the output variant (does not short-circuit) must be the _first_ variant
+//!
+//! ## Example Usage:
+//! ```rust
+//! #![feature(never_type)]
+//! #![feature(try_trait_v2)]
+//! use try_v2::Try;
+//!
+//! #[derive(Try)]
+//! enum TestResult<T> {
+//!     Ok(T),
+//!     TestsFailed,
+//!     OtherError(String)
+//! }
+//!
+//! fn run_tests() -> TestResult<()> {
+//!     TestResult::OtherError("oops!".to_string())?; // <- Function short-circuits here ...
+//!     TestResult::TestsFailed?;
+//!     TestResult::Ok(())
+//! }
+//!
+//! assert!(matches!(run_tests(), TestResult::OtherError(msg) if msg == "oops!"))
+//! ```
 use proc_macro::TokenStream as TokenStream1;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use syn::{Data, DeriveInput, GenericParam};
 
 #[proc_macro_derive(Try)]
+/// Derives [try_trait_v2](https://rust-lang.github.io/rfcs/3058-try-trait-v2.html)
+///
+/// ## Requires:
+///   - `RUSTC_BOOTSRAP = 1` (or nightly)
+///   - `#![feature(never_type)]`
+///   - `#![feature(try_trait_v2)]`
+///
+/// ## Current Limitations on the annotated type:
+///   - must be an `enum`
+///   - must have _at least one_ generic type
+///   - the _first_ generic type must be the `Output` type (produced when not short circuiting)
+///   - the output variant (does not short-circuit) must be the _first_ variant
 pub fn try_trait_v2_derive(input: TokenStream1) -> TokenStream1 {
     impl_try_trait_v2(input.into()).into()
 }
