13ae72fc574acdc7f270bc23ed13e7f97b8542a2
Mike Foster <i.am.mike.foster@gmail.com>
2026-03-27 14:37:05 +0000

    fix test

--- tests/compilation/pass_lifetime_conversion.rs
+++ tests/compilation/pass_lifetime_conversion.rs
@@ -13,20 +13,19 @@ enum BorrowedResult<'t, 'e, T, E> {
     Err(&'e E),
 }
 
-type BorrowedResultResidual = BorrowedResult<'a, 'e, !, i32>;
-
-type Result<'o, 'f> = std::result::Result<&'o i32, Failure<'f>>;
+type StdResult<'o, 'f> = std::result::Result<&'o i32, Failure<'f>>;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Failure<'f>(&'f i32);
 
-impl<'a, 'e, 'f> From<BorrowedResultResidual> for Failure<'f>
+impl<'t, 'e, 'f, E> From<BorrowedResult<'t, 'e, !, E>> for Failure<'f>
 where
     'e: 'f,
+    &'e E: Into<&'f i32>,
 {
-    fn from(res: BorrowedResultResidual) -> Self {
+    fn from(res: BorrowedResult<'t, 'e, !, E>) -> Self {
         match res {
-            BorrowedResult::Err(e) => Failure(e),
+            BorrowedResult::Err(e) => Failure(e.into()),
             BorrowedResult::Ok(&never) => match never {},
         }
     }
@@ -41,7 +40,7 @@ where
     }
 }
 
-fn borrowed_to_result_passthrough<'t, 'e>(okval: &'t i32, errval: &'e i32) -> Result<'t, 'e> {
+fn borrowed_to_result_passthrough<'t, 'e>(okval: &'t i32, errval: &'e i32) -> StdResult<'t, 'e> {
     let rtn = match errval {
         ..=4 => BorrowedResult::Ok(okval)?,
         5 => BorrowedResult::Err(errval)?,
@@ -50,10 +49,13 @@ fn borrowed_to_result_passthrough<'t, 'e>(okval: &'t i32, errval: &'e i32) -> Re
     Ok(rtn)
 }
 
-fn borrowed_to_result_restricted<'t, 'e, 'o, 'f>(okval: &'t i32, errval: &'e i32) -> Result<'o, 'f>
+fn borrowed_to_result_restricted<'t, 'e, 'o, 'f>(
+    okval: &'t i32,
+    errval: &'e i32,
+) -> StdResult<'o, 'f>
 where
-    'o: 't,
-    'f: 'e,
+    't: 'o,
+    'e: 'f,
 {
     let rtn = match errval {
         ..=4 => BorrowedResult::Ok(okval)?,
@@ -68,7 +70,7 @@ fn result_to_borrowed_passthrough<'o, 'f>(
     errval: &'f i32,
 ) -> BorrowedResult<'o, 'f, i32, i32> {
     let rtn = match errval {
-        ..=4 => Ok(okval)?,
+        ..=4 => Ok::<_, Failure<'f>>(okval)?,
         5 => Err(Failure(errval))?,
         6.. => Err(Failure(errval))?,
     };
@@ -80,11 +82,11 @@ fn result_to_borrowed_restricted<'o, 'f, 't, 'e>(
     errval: &'f i32,
 ) -> BorrowedResult<'t, 'e, i32, i32>
 where
-    't: 'o,
-    'e: 'e,
+    'o: 't,
+    'f: 'e,
 {
     let rtn = match errval {
-        ..=4 => Ok(okval)?,
+        ..=4 => Ok::<_, Failure<'f>>(okval)?,
         5 => Err(Failure(errval))?,
         6.. => Err(Failure(errval))?,
     };
@@ -92,24 +94,24 @@ where
 }
 
 fn main() {
-    assert_matches!(borrowed_to_result_passthrough(&0, &1), Result::Ok(&0));
+    assert_matches!(borrowed_to_result_passthrough(&0, &1), StdResult::Ok(&0));
     assert_matches!(
         borrowed_to_result_passthrough(&0, &5),
-        Result::Err(Failure(&5))
+        StdResult::Err(Failure(&5))
     );
     assert_matches!(
         borrowed_to_result_passthrough(&0, &7),
-        Result::Err(Failure(&7))
+        StdResult::Err(Failure(&7))
     );
 
-    assert_matches!(borrowed_to_result_restricted(&0, &1), Result::Ok(&0));
+    assert_matches!(borrowed_to_result_restricted(&0, &1), StdResult::Ok(&0));
     assert_matches!(
         borrowed_to_result_restricted(&0, &5),
-        Result::Err(Failure(&5))
+        StdResult::Err(Failure(&5))
     );
     assert_matches!(
         borrowed_to_result_restricted(&0, &7),
-        Result::Err(Failure(&7))
+        StdResult::Err(Failure(&7))
     );
 
     assert_matches!(
