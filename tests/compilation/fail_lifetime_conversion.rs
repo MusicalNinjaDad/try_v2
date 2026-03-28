0a90d375eaa11dc59cc2efd3d86261bafce7dc36
Mike Foster <i.am.mike.foster@gmail.com>
2026-03-28 11:21:34 +0000

    use functions in fail conversion

--- tests/compilation/fail_lifetime_conversion.rs
+++ tests/compilation/fail_lifetime_conversion.rs
@@ -11,17 +11,58 @@ enum BorrowedResult<'t, 'e, T, E> {
     Err(&'e E),
 }
 
+impl<'pass, 'fail, 't, 'e, T, E> BorrowedResult<'t, 'e, T, E>
+where
+    'pass: 't,
+    'fail: 'e,
+{
+    fn fail(err: &'fail E) -> Self {
+        let r = Self::Err(err)?;
+        Self::Ok(r)
+    }
+
+    fn fail_directly(err: &'fail E) -> Self {
+        Self::Err(err)
+    }
+
+    fn pass(val: &'pass T) -> Self {
+        Self::Ok(val)
+    }
+}
+
 type StdResult<'o, 'f> = std::result::Result<&'o i32, Failure<'f>>;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct Failure<'f>(&'f i32);
 
-impl<'a, 'e, 'f, E> From<BorrowedResult<'a, 'e, !, E>> for Failure<'f>
+fn fail<'fail, 'o, 'f>(err: Failure<'fail>) -> StdResult<'o, 'f>
+where
+    'fail: 'f,
+{
+    let r = Err(err)?;
+    Ok(r)
+}
+
+fn fail_directly<'fail, 'o, 'f>(err: Failure<'fail>) -> StdResult<'o, 'f>
+where
+    'fail: 'f,
+{
+    Err(err)
+}
+
+fn pass<'pass, 'o, 'f>(val: &'pass i32) -> StdResult<'o, 'f>
+where
+    'pass: 'o,
+{
+    Ok(val)
+}
+
+impl<'t, 'e, 'f, E> From<BorrowedResult<'t, 'e, !, E>> for Failure<'f>
 where
     'e: 'f,
     &'e E: Into<&'f i32>,
 {
-    fn from(res: BorrowedResult<'a, 'e, !, E>) -> Self {
+    fn from(res: BorrowedResult<'t, 'e, !, E>) -> Self {
         match res {
             BorrowedResult::Err(e) => Failure(e.into()),
             BorrowedResult::Ok(&never) => match never {},
@@ -46,9 +87,9 @@ where
     'e: 'f,
 {
     let rtn = match errval {
-        ..=4 => BorrowedResult::Ok(okval)?,
-        5 => BorrowedResult::Err(errval)?,
-        6.. => BorrowedResult::Err(errval)?,
+        ..=4 => BorrowedResult::pass(okval)?,
+        5 => BorrowedResult::fail(errval)?,
+        6.. => BorrowedResult::fail_directly(errval)?,
     };
     Ok(rtn)
 }
@@ -61,9 +102,9 @@ where
     't: 'o,
 {
     let rtn = match errval {
-        ..=4 => BorrowedResult::Ok(okval)?,
-        5 => BorrowedResult::Err(errval)?,
-        6.. => BorrowedResult::Err(errval)?,
+        ..=4 => BorrowedResult::pass(okval)?,
+        5 => BorrowedResult::fail(errval)?,
+        6.. => BorrowedResult::fail_directly(errval)?,
     };
     Ok(rtn)
 }
@@ -76,9 +117,9 @@ where
     'f: 'e,
 {
     let rtn = match errval {
-        ..=4 => Ok::<_, Failure<'f>>(okval)?,
-        5 => Err(Failure(errval))?,
-        6.. => Err(Failure(errval))?,
+        ..=4 => pass(okval)?,
+        5 => fail(Failure(errval))?,
+        6.. => fail_directly(Failure(errval))?,
     };
     BorrowedResult::Ok(rtn)
 }
@@ -91,9 +132,9 @@ where
     'o: 't,
 {
     let rtn = match errval {
-        ..=4 => Ok::<_, Failure<'f>>(okval)?,
-        5 => Err(Failure(errval))?,
-        6.. => Err(Failure(errval))?,
+        ..=4 => pass(okval)?,
+        5 => fail(Failure(errval))?,
+        6.. => fail_directly(Failure(errval))?,
     };
     BorrowedResult::Ok(rtn)
 }
