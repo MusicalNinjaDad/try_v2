711ff3faca793933de63dc3f625383022e44658e
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-10 07:46:07 +0000

    meaningful test showing real-life case: TryFrom

--- tests/compilation/pass_ResultMeBang.rs
+++ tests/compilation/pass_ResultMeBang.rs
@@ -2,7 +2,7 @@
 #![feature(try_trait_v2)]
 #![feature(try_trait_v2_residual)]
 
-use try_v2::{Try,Try_ConvertResult};
+use try_v2::{Try, Try_ConvertResult};
 
 #[derive(Try, Try_ConvertResult)]
 #[must_use]
@@ -11,17 +11,23 @@ enum Eightball<T> {
     No,
 }
 
-fn even(num: i32) -> Result<i32, Eightball<!>> {
-    if num % 2 == 0 {
-        Result::Ok(num)
-    } else {
-        Result::Err(Eightball::No)
+struct Even(i32);
+
+impl TryFrom<i32> for Even {
+    type Error = Eightball<!>;
+
+    fn try_from(num: i32) -> Result<Even, Eightball<!>> {
+        if num % 2 == 0 {
+            Result::Ok(Even(num))
+        } else {
+            Result::Err(Eightball::No)
+        }
     }
 }
 
 fn even_string(num: i32) -> Eightball<String> {
-    let n = even(num)?;
-    let s = format!("{n}");
+    let n = Even::try_from(num)?;
+    let s = format!("{}", n.0);
     Eightball::Yes(s)
 }
 
