f3c5a7451c4e4a41b910795fdfc336a6aae57d2b
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-10 15:30:13 +0000

    prepare test for Box

---
+++ tests/pass_box.rs
@@ -0,0 +1,12 @@
+#![feature(never_type)]
+#![feature(try_trait_v2)]
+#![feature(try_trait_v2_residual)]
+
+use try_v2::Try;
+
+#[derive(Try)]
+#[must_use]
+enum Eightball<Y> {
+    Yes(Box<Y>),
+    No,
+}
