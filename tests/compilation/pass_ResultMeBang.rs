79a339dc2ddec681329d800b5b37beac10ab09aa
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-10 07:46:41 +0000

    don't derive ConvertResult

--- tests/compilation/pass_ResultMeBang.rs
+++ tests/compilation/pass_ResultMeBang.rs
@@ -2,9 +2,9 @@
 #![feature(try_trait_v2)]
 #![feature(try_trait_v2_residual)]
 
-use try_v2::{Try, Try_ConvertResult};
+use try_v2::Try;
 
-#[derive(Try, Try_ConvertResult)]
+#[derive(Try)]
 #[must_use]
 enum Eightball<T> {
     Yes(T),
