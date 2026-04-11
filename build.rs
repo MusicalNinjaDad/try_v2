4af34d1b709389fe2d712ee845e045dada0c208a
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-11 17:09:40 +0000

    revert back to b3ebb379465c05b451c807480a28675d535db676 & remove final clippy --tests which really doesn't want to run in CI

--- build.rs
+++ build.rs
@@ -35,16 +35,6 @@ fn assert_matches_in_root(ac: &AutoCfg) {
     #![feature(assert_matches)]
     use std::assert_matches;
 
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn to_allow_clippy_to_lint_tests() {
-            assert_matches!(Some(4), Some(_));
-        }
-    }
-
     fn main() {
         assert_matches!(Some(4), Some(_));
     }
@@ -61,16 +51,6 @@ fn assert_matches_in_module(ac: &AutoCfg) {
     #![allow(stable_features)]
     #![feature(assert_matches)]
     use std::assert_matches::assert_matches;
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn to_allow_clippy_to_lint_tests() {
-            assert_matches!(Some(4), Some(_));
-        }
-    }
 
     fn main() {
         assert_matches!(Some(4), Some(_));
