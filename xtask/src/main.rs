4dc75553377d7ee3bc18af100799c38b78cebe16
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-12 14:35:31 +0000

    basic shape for main

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,1 +1,47 @@
-fn main() {}
+#![feature(never_type)]
+#![feature(try_trait_v2)]
+#![feature(try_trait_v2_residual)]
+
+use std::process::Termination as _T;
+
+use clap::{Parser, Subcommand};
+use exit_safely::Termination;
+use try_v2::{Try, Try_ConvertResult};
+
+#[derive(Debug, Termination, Try, Try_ConvertResult)]
+#[repr(u8)]
+#[must_use]
+enum Exit<T: _T> {
+    Ok(T) = 0,
+    InvocationError(Box<clap::Error>) = 2,
+}
+
+impl<T: _T> From<clap::Error> for Exit<T> {
+    fn from(e: clap::Error) -> Self {
+        Self::InvocationError(Box::new(e))
+    }
+}
+
+#[derive(Parser)]
+#[command(version)]
+struct XTask {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// git add if all is good
+    Add,
+}
+
+fn main() -> Exit<()> {
+    let xtask = XTask::try_parse()?;
+
+    match &xtask.command {
+        Command::Add => {
+            fmt()?;
+            add()?;
+        }
+    }
+}
