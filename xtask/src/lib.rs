3d662fec60628f343d77d442e50805743c6110c6
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-12 15:15:38 +0000

    fmt

--- xtask/src/lib.rs
+++ xtask/src/lib.rs
@@ -1,4 +1,8 @@
-use std::{io, path::Path, process::{Command, Output}};
+use std::{
+    io,
+    path::Path,
+    process::{Command, Output},
+};
 
 pub fn fmt(root: &Path) -> Result<Output, io::Error> {
     Command::new("cargo").current_dir(root).arg("fmt").output()
