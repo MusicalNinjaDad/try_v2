63f9481a033b5cd5d1914e0816a86b42cd9aa02e
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-16 07:40:40 +0000

    shrink example

--- examples/TryFrom2.rs
+++ examples/TryFrom2.rs
@@ -16,31 +16,15 @@ struct Even(i32);
 
 struct Even2(i32);
 
-impl TryFrom<i32> for Even {
-    type Error = Eightball<!>;
-
-    fn try_from(num: i32) -> Result<Even, Eightball<!>> {
-        if num % 2 == 0 {
-            Result::Ok(Even(num))
-        } else {
-            Result::Err(Eightball::No)
-        }
-    }
-}
-
-fn even_string(num: i32) -> Eightball<String> {
-    let n = Even::try_from(num)?;
-    let s = format!("{}", n.0);
-    Eightball::Yes(s)
-}
-
-trait TryFrom2<T> {
-    type Return: std::ops::Try;
+trait TryFrom2<T>: std::marker::Sized {
+    type Error;
+    type Return: std::ops::Try = Result<Self, Self::Error>;
 
     fn try_from2(value: T) -> Self::Return;
 }
 
 impl TryFrom2<i32> for Even {
+    type Error = ();
     type Return = Eightball<Self>;
 
     fn try_from2(num: i32) -> Self::Return {
@@ -52,36 +36,10 @@ impl TryFrom2<i32> for Even {
     }
 }
 
-fn even_string2(num: i32) -> Eightball<String> {
-    let n = Even::try_from2(num)?;
-    let s = format!("{}", n.0);
-    Eightball::Yes(s)
-}
-
-trait TryFrom3<T>: std::marker::Sized {
-    type Error;
-    type Return: std::ops::Try = Result<Self, Self::Error>;
-
-    fn try_from3(value: T) -> Self::Return;
-}
-
-impl TryFrom3<i32> for Even {
-    type Error = ();
-    type Return = Eightball<Self>;
-
-    fn try_from3(num: i32) -> Self::Return {
-        if num % 2 == 0 {
-            Eightball::Yes(Even(num))
-        } else {
-            Eightball::No
-        }
-    }
-}
-
-impl TryFrom3<i32> for Even2 {
+impl TryFrom2<i32> for Even2 {
     type Error = Eightball<!>;
 
-    fn try_from3(num: i32) -> Result<Even2, Eightball<!>> {
+    fn try_from2(num: i32) -> Result<Even2, Eightball<!>> {
         if num % 2 == 0 {
             Result::Ok(Even2(num))
         } else {
@@ -90,14 +48,14 @@ impl TryFrom3<i32> for Even2 {
     }
 }
 
-fn even_string3(num: i32) -> Eightball<String> {
-    let n = Even::try_from3(num)?;
+fn even_string_own_try_type(num: i32) -> Eightball<String> {
+    let n = Even::try_from2(num)?;
     let s = format!("{}", n.0);
     Eightball::Yes(s)
 }
 
-fn even_string3_via_result(num: i32) -> Eightball<String> {
-    let n = Even2::try_from3(num)?;
+fn even_string_via_result(num: i32) -> Eightball<String> {
+    let n = Even2::try_from2(num)?;
     let s = format!("{}", n.0);
     Eightball::Yes(s)
 }
@@ -105,10 +63,10 @@ fn even_string3_via_result(num: i32) -> Eightball<String> {
 struct TryFromIntError; // Cannot instantiate a std::num::TryFromIntError
 
 /// Non-breaking: this is identical (text) to std impl
-impl TryFrom3<i8> for u8 {
+impl TryFrom2<i8> for u8 {
     type Error = TryFromIntError;
 
-    fn try_from3(u: i8) -> Result<Self, Self::Error> {
+    fn try_from2(u: i8) -> Result<Self, Self::Error> {
         if u >= 0 {
             Ok(u as Self)
         } else {
@@ -118,18 +76,12 @@ impl TryFrom3<i8> for u8 {
 }
 
 fn main() {
-    assert!(matches!(even_string(2), Eightball::Yes(s) if s == "2"));
-    assert!(matches!(even_string(1), Eightball::No));
+    assert!(matches!(even_string_own_try_type(2), Eightball::Yes(s) if s == "2"));
+    assert!(matches!(even_string_own_try_type(1), Eightball::No));
 
-    assert!(matches!(even_string2(2), Eightball::Yes(s) if s == "2"));
-    assert!(matches!(even_string2(1), Eightball::No));
+    assert!(matches!(even_string_via_result(2), Eightball::Yes(s) if s == "2"));
+    assert!(matches!(even_string_via_result(1), Eightball::No));
 
-    assert!(matches!(even_string3(2), Eightball::Yes(s) if s == "2"));
-    assert!(matches!(even_string3(1), Eightball::No));
-
-    assert!(matches!(even_string3_via_result(2), Eightball::Yes(s) if s == "2"));
-    assert!(matches!(even_string3_via_result(1), Eightball::No));
-
-    assert!(matches!(u8::try_from3(5), Ok(5)));
-    assert!(matches!(u8::try_from3(-1), Err(TryFromIntError)));
+    assert!(matches!(u8::try_from2(5), Ok(5)));
+    assert!(matches!(u8::try_from2(-1), Err(TryFromIntError)));
 }
