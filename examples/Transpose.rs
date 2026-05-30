2f92a6a883c62dce1c61516f18302a16b346a4c7
Mike Foster <i.am.mike.foster@gmail.com>
2026-05-30 10:39:19 +0000

    extend tests

--- examples/Transpose.rs
+++ examples/Transpose.rs
@@ -50,11 +50,45 @@ fn ok_some() {
     assert_eq!(stdlib, custom)
 }
 
+
+#[test]
+fn ok_none() {
+    let ok_none: Result<Option<u32>, String> = Ok(None);
+    let stdlib = ok_none.clone().transpose();
+    let custom = ok_none.transpose2();
+    assert_eq!(stdlib, custom)
+}
+
+#[test]
+fn err() {
+    let err: Result<Option<i32>, String> = Err("Oops".to_string());
+    let stdlib = err.clone().transpose();
+    let custom = err.transpose2();
+    assert_eq!(stdlib, custom)
+}
+
+
 #[test]
 fn some_ok() {
     let some_ok: Option<Result<u32, String>> =Some(Ok(5));
     let stdlib: Result<Option<u32>, String> = some_ok.clone().transpose();
     let custom: Result<Option<u32>, String> = some_ok.transpose2();
+    assert_eq!(stdlib, custom)
+}
+
+#[test]
+fn some_err() {
+    let some_err: Option<Result<u32, String>> = Some(Err("Oops".to_string()));
+    let stdlib = some_err.clone().transpose();
+    let custom = some_err.transpose2();
+    assert_eq!(stdlib, custom)
+}
+
+#[test]
+fn none() {
+    let none: Option<Result<u32, String>> = None;
+    let stdlib = none.clone().transpose();
+    let custom = none.transpose2();
     assert_eq!(stdlib, custom)
 }
 
