72fc1faca5a112f7236808a1dace1dc05484b537
Mike Foster <i.am.mike.foster@gmail.com>
2026-05-30 08:49:48 +0000

    handle transpose Err case via Try

--- examples/Transpose.rs
+++ examples/Transpose.rs
@@ -14,8 +14,11 @@ where
 
 impl Transpose<Option<Result<u32, String>>> for Result<Option<u32>, String> {
     fn transpose2(self) -> Option<Result<u32, String>> {
+        type T = Result<Option<u32>, String>;
+        type TR = <T as Try>::Residual; // Result<!, String>
         type U = Option<Result<u32, String>>;
-        type UR = <U as Try>::Residual;
+        type UO = <U as Try>::Output; // Result<u32, String>
+        type UR = <U as Try>::Residual; // Option<!>
         let opt_or_err = self.branch();
         match opt_or_err {
             std::ops::ControlFlow::Continue(opt) => match opt.branch() {
@@ -28,8 +31,8 @@ impl Transpose<Option<Result<u32, String>>> for Result<Option<u32>, String> {
                 }
             },
             std::ops::ControlFlow::Break(err) => {
-                let Err(err) = err;
-                Some(Err(err))
+                let inner_result = <UO as FromResidual<TR>>::from_residual(err);
+                U::from_output(inner_result)
             }
         }
     }
