de2d75344e05598a5be913e153b28da1f7acc7e2
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-02 14:33:32 +0000

    unwrap_or...

--- crates/try_v2/src/extract.rs
+++ crates/try_v2/src/extract.rs
@@ -44,11 +44,52 @@ where
     fn expect<T>(self, msg: &str) -> T
     where
         Self: Try<Output = T>,
+        Self::Residual: Debug,
     {
         match self.branch() {
             ControlFlow::Continue(v) => v,
             #[cfg(not(panic = "immediate-abort"))]
-            ControlFlow::Break(_) => panic!("{msg}"),
+            ControlFlow::Break(r) => panic!("{msg}: {r:?}"),
+            #[cfg(panic = "immediate-abort")]
+            ControlFlow::Break(_) => panic!(),
+        }
+    }
+
+    /// Return the contained value or the given default.
+    ///
+    /// Arguments passed to unwrap_or are eagerly evaluated; if you are passing the result of a
+    /// function call, it is recommended to use unwrap_or_else, which is lazily evaluated.
+    fn unwrap_or<T>(self, default: T) -> T
+    where
+        Self: Try<Output = T>,
+    {
+        match self.branch() {
+            ControlFlow::Continue(v) => v,
+            ControlFlow::Break(_) => default,
+        }
+    }
+
+    /// Return the contained value or a default. Requires `T` to implement `Default`
+    fn unwrap_or_default<T>(self) -> T
+    where
+        Self: Try<Output = T>,
+        T: Default,
+    {
+        match self.branch() {
+            ControlFlow::Continue(v) => v,
+            ControlFlow::Break(_) => Default::default(),
+        }
+    }
+
+    /// Return the contained value or a value computed from a closure.
+    fn unwrap_or_else<T, F>(self, f: F) -> T
+    where
+        Self: Try<Output = T>,
+        F: FnOnce() -> T,
+    {
+        match self.branch() {
+            ControlFlow::Continue(v) => v,
+            ControlFlow::Break(_) => f(),
         }
     }
 }
