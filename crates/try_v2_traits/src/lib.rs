ec58fe09146738fb946470dfb5a64ffeab182e93
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-12 10:28:40 +0000

    init separate crate

---
+++ crates/try_v2_traits/src/lib.rs
@@ -0,0 +1,12 @@
+#![cfg(all(has_try_trait_v2, has_try_trait_v2_residual))]
+#![cfg_attr(unstable_try_trait_v2, feature(try_trait_v2))]
+#![cfg_attr(unstable_try_trait_v2, feature(try_trait_v2_residual))]
+#![cfg_attr(all(test, unstable_option_zip), feature(option_zip))]
+
+mod transform;
+#[doc(inline)]
+pub use transform::Transform;
+
+mod extract;
+#[doc(inline)]
+pub use extract::Extract;
