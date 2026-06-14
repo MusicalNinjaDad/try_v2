3e3c6bf3b7da1a58cb2b1ca9f2abd492a2e726f9
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-14 10:00:01 +0000

    poll option example

---
+++ crates/try_v2_derive/examples/poll_option.rs
@@ -0,0 +1,12 @@
+#![allow(unused)]
+#![allow(clippy::disallowed_names)]
+
+use std::task::{Poll, ready};
+
+fn wobble() -> Poll<Option<Result<(),()>>> {
+    let foo: Poll<Option<Result<(),()>>> = Poll::Ready(Some(Ok(())));
+    let bar = ready!(foo?);
+    todo!()
+}
+
+fn main() {}
