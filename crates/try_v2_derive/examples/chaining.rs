bcc5e2cd8a86a62e80908c4188d2372cd3181e61
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-14 12:54:31 +0000

    lose the compiler error on chaining

--- crates/try_v2_derive/examples/chaining.rs
+++ crates/try_v2_derive/examples/chaining.rs
@@ -1,12 +1,15 @@
 #![feature(try_trait_v2)]
+#![allow(unused)]
 use std::ops::{FromResidual, Try};
 
-impl<T,X,Y> FromResidual<Y::Residual> for X 
-where 
-X: Try<Output = Y>, // `Foo<Bar<T>>`
-Y: Try<Output = T>, // `Bar<T>`
-{
-    fn from_residual(residual: Y::Residual) -> Self {
-        Try::from_output(FromResidual::from_residual(residual))
-    }
-}
+// impl<T,X,Y> FromResidual<Y::Residual> for X
+// where
+// X: Try<Output = Y>, // `Foo<Bar<T>>`
+// Y: Try<Output = T>, // `Bar<T>`
+// {
+//     fn from_residual(residual: Y::Residual) -> Self {
+//         Try::from_output(FromResidual::from_residual(residual))
+//     }
+// }
+
+fn main() {}
