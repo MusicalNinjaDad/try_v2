8b52478297c7c07c43430813e1ab2c141d56819f
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-16 08:04:29 +0000

    move passing compile tests to examples

--- crates/try_v2_derive/tests/test_compilation.rs
+++ crates/try_v2_derive/tests/test_compilation.rs
@@ -4,8 +4,3 @@ use trybuild::TestCases;
 fn errors() {
     TestCases::new().compile_fail("tests/compilation/examples/fail_*.rs");
 }
-
-#[test]
-fn valid() {
-    TestCases::new().pass("tests/compilation/examples/pass_*.rs");
-}
