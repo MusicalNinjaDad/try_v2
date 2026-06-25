cd6debd3f1ed63f32b93729160efa0c3532f676c
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-25 16:14:54 +0000

    sketch out usage in example

---
+++ crates/try_v2_derive/examples/FromResidualResult.rs
@@ -0,0 +1,38 @@
+#![feature(never_type)]
+#![feature(try_trait_v2)]
+#![feature(try_trait_v2_residual)]
+
+use try_v2_derive::{FromResidual, Try};
+
+#[derive(Debug, Try, FromResidual, PartialEq)]
+#[FromResidual(Result<_, Self::Residual>)]
+#[must_use]
+enum EightBall<Y, N> {
+    Yes(Y),
+    RollAgain,
+    No(N),
+}
+
+impl<N> From<EightBall<!, N>> for Option<N> {
+    fn from(residual: EightBall<!, N>) -> Self {
+        match residual {
+            EightBall::RollAgain => None,
+            EightBall::No(n) => Some(n),
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn eightball_error() -> Result<(), Option<EightBall<!, ()>>> {
+    let _ = EightBall::RollAgain?;
+    let _ = EightBall::Yes(())?;
+    Ok(())
+}
+
+#[test]
+fn test_maybe_eightball() {
+    let ret = maybe_eightball();
+    assert_eq!(ret, Some(EightBall::RollAgain));
+}
+
+fn main() {}
