07333709fe05b8c271f7116a8d26be81cfa9355e
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-26 06:04:31 +0000

    show unconstrained type problem

--- crates/try_v2_derive/examples/FromResidualResult.rs
+++ crates/try_v2_derive/examples/FromResidualResult.rs
@@ -2,15 +2,33 @@
 #![feature(try_trait_v2)]
 #![feature(try_trait_v2_residual)]
 
+use std::ops::Residual;
+
 use try_v2_derive::{FromResidual, Try};
 
-#[derive(Debug, Try, FromResidual, PartialEq)]
-#[FromResidual(Result<_, Self::Residual>)]
+#[derive(Debug, Try, PartialEq)]
+// #[FromResidual(Result<_, Self::Residual>)]
 #[must_use]
 enum EightBall<Y, N> {
     Yes(Y),
     RollAgain,
     No(N),
+}
+
+impl<
+    Y,
+    N,
+    Derive_TryConvert_ResultT,
+    Derive_TryConvert_ResultE,
+> ::std::ops::FromResidual<<EightBall<Y,N> as std::ops::Try>::Residual>
+    for ::std::result::Result<Derive_TryConvert_ResultT, Derive_TryConvert_ResultE>
+    where <EightBall<Y,N> as ::std::ops::Try>::Residual: Into<Derive_TryConvert_ResultE>,
+{
+    #[inline]
+    #[track_caller]
+    fn from_residual(residual: <EightBall<Y, N> as ::std::ops::Try>::Residual) -> Self {
+        ::std::result::Result::Err(residual.into())
+    }
 }
 
 impl<N> From<EightBall<!, N>> for Option<N> {
@@ -24,8 +42,8 @@ impl<N> From<EightBall<!, N>> for Option<N> {
 
 #[allow(dead_code)]
 fn eightball_error() -> Result<(), Option<EightBall<!, ()>>> {
-    let _ = EightBall::RollAgain?;
-    let _ = EightBall::Yes(())?;
+    let _ = EightBall::<_, ()>::RollAgain?;
+    let _ = EightBall::<_, ()>::Yes(())?;
     Ok(())
 }
 
