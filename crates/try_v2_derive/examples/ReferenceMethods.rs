788ca3d6d1643ca02f408e0535d79f08c7738d34
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-28 09:24:21 +0000

    manual as_deref

--- crates/try_v2_derive/examples/ReferenceMethods.rs
+++ crates/try_v2_derive/examples/ReferenceMethods.rs
@@ -3,7 +3,7 @@
 #![feature(try_trait_v2)]
 #![feature(try_trait_v2_residual)]
 
-use std::ops::Try;
+use std::ops::{Deref, Try};
 
 use try_v2_derive::Try;
 
@@ -14,6 +14,16 @@ enum EightBall<Y, N> {
     Yes(Y),
     RollAgain,
     No(N),
+}
+
+impl<Y: Deref,N> EightBall<Y,N> {
+    fn as_deref(&self) -> EightBall<&<Y as Deref>::Target, &N> {
+        match self {
+            EightBall::Yes(y) => EightBall::Yes(y),
+            EightBall::RollAgain => EightBall::RollAgain,
+            EightBall::No(n) => EightBall::No(n),
+        }
+    }
 }
 
 #[test]
@@ -41,6 +51,22 @@ fn refs() {
     assert_eq!(
         EightBall::<&mut i32, &mut i32>::No(&mut 5),
         EightBall::<i32, i32>::No(5).as_mut()
+    );
+}
+
+#[test]
+fn derefs() {
+    assert_eq!(
+        EightBall::<&str, &i32>::Yes("yes"),
+        EightBall::<String, i32>::Yes("yes".to_string()).as_deref()
+    );
+    assert_eq!(
+        EightBall::<&str, &i32>::RollAgain,
+        EightBall::<String, i32>::RollAgain.as_deref()
+    );
+    assert_eq!(
+        EightBall::<&str, &i32>::No(&5),
+        EightBall::<String, i32>::No(5).as_deref()
     );
 }
 
