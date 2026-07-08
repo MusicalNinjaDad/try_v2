fb74682c4d0f92514c94d8b4016b56899c169605
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-08 16:43:30 +0000

    remove let_chains stabilised before msrv

--- crates/try_v2_derive/build.rs
+++ crates/try_v2_derive/build.rs
@@ -5,7 +5,6 @@ fn main() -> Result<()> {
 
     let allowed_features = cargo_allowed_features()?;
     ac.emit_unstable_feature(assert_matches, &allowed_features);
-    ac.emit_unstable_feature(OtherFeature("let_chains".to_string()), &allowed_features); // stable 1.88.0 https://github.com/rust-lang/rust/issues/53667
     ac.emit_unstable_feature(iterator_try_collect, &allowed_features);
     ac.emit_unstable_feature(OtherFeature("if_let_guard".to_string()), &allowed_features); // stable 1.95.0 https://github.com/rust-lang/rust/issues/51114
     ac.emit_unstable_feature(never_type, &allowed_features);
