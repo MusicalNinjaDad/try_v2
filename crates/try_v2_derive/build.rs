3efd1da6ddb2ff40fb11e7132e00062149b34abd
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-08 16:38:40 +0000

    update build scripts

--- crates/try_v2_derive/build.rs
+++ crates/try_v2_derive/build.rs
@@ -1,18 +1,15 @@
-use ninja_build_rs::{
-    Result,
-    nightly::{Nightly, cargo_allowed_features},
-};
+use ninja_build_rs::prelude::*;
 
 fn main() -> Result<()> {
     let ac = autocfg::new();
 
     let allowed_features = cargo_allowed_features()?;
-    ac.emit_unstable_feature("assert_matches", &allowed_features);
-    ac.emit_unstable_feature("let_chains", &allowed_features); // stable 1.88.0 https://github.com/rust-lang/rust/issues/53667
-    ac.emit_unstable_feature("iterator_try_collect", &allowed_features);
-    ac.emit_unstable_feature("if_let_guard", &allowed_features); // stable 1.95.0 https://github.com/rust-lang/rust/issues/51114
-    ac.emit_unstable_feature("never_type", &allowed_features);
-    ac.emit_unstable_feature("try_trait_v2", &allowed_features);
-    ac.emit_unstable_feature("try_trait_v2_residual", &allowed_features);
+    ac.emit_unstable_feature(assert_matches, &allowed_features);
+    ac.emit_unstable_feature(OtherFeature("let_chains".to_string()), &allowed_features); // stable 1.88.0 https://github.com/rust-lang/rust/issues/53667
+    ac.emit_unstable_feature(iterator_try_collect, &allowed_features);
+    ac.emit_unstable_feature(OtherFeature("if_let_guard".to_string()), &allowed_features); // stable 1.95.0 https://github.com/rust-lang/rust/issues/51114
+    ac.emit_unstable_feature(never_type, &allowed_features);
+    ac.emit_unstable_feature(try_trait_v2, &allowed_features);
+    ac.emit_unstable_feature(try_trait_v2_residual, &allowed_features);
     Ok(())
 }
--- crates/try_v2_traits/build.rs
+++ crates/try_v2_traits/build.rs
@@ -1,14 +1,11 @@
-use ninja_build_rs::{
-    Result,
-    nightly::{Nightly, cargo_allowed_features},
-};
+use ninja_build_rs::prelude::*;
 
 fn main() -> Result<()> {
     let ac = autocfg::new();
 
     let allowed_features = cargo_allowed_features()?;
-    ac.emit_unstable_feature("try_trait_v2", &allowed_features);
-    ac.emit_unstable_feature("try_trait_v2_residual", &allowed_features);
-    ac.emit_unstable_feature("option_zip", &allowed_features);
+    ac.emit_unstable_feature(try_trait_v2, &allowed_features);
+    ac.emit_unstable_feature(try_trait_v2_residual, &allowed_features);
+    ac.emit_unstable_feature(OtherFeature("option_zip".to_string()), &allowed_features);
     Ok(())
 }
