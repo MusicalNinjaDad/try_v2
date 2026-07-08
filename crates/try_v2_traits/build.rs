e459405fe9c3d6858e038ed290ab5ded17412ef1
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-08 16:44:12 +0000

    explain use of option_zip

--- crates/try_v2_traits/build.rs
+++ crates/try_v2_traits/build.rs
@@ -6,6 +6,6 @@ fn main() -> Result<()> {
     let allowed_features = cargo_allowed_features()?;
     ac.emit_unstable_feature(try_trait_v2, &allowed_features);
     ac.emit_unstable_feature(try_trait_v2_residual, &allowed_features);
-    ac.emit_unstable_feature(OtherFeature("option_zip".to_string()), &allowed_features);
+    ac.emit_unstable_feature(OtherFeature("option_zip".to_string()), &allowed_features); // only used in tests
     Ok(())
 }
